54f14eb44b8e4efd7efc6c7ee0fbe7d9e12b42b7
Nikita Kononenko <jadnostt@gmail.com>
2024-08-30 01:38:29 -0700

    fix[i18n]: uk_UA

---
+++ src/processes/i18n/uk_UA/index.ts
@@ -0,0 +1,13 @@
+import footer from './footer.json';
+import common from './common.json';
+import hero from './hero.json';
+import header from './header.json';
+import index from './index.json';
+
+export default {
+    common: common,
+    footer: footer,
+    header: header,
+    hero: hero,
+    index: index
+}
